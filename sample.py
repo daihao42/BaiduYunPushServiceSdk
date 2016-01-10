a817259f4041b9970b34ea144688b76f76b5e8e2
daihaovigg <865058222@qq.com>
2016-01-10 20:59:35 +0800

    init

---
+++ sample.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# _*_ coding: UTF-8 _*_
+
+import sys
+import time
+from Channel import *
+
+channel_id = '3582618851681887324' #android
+msg = '{"title":"Message from Push","description":"hello world"}'
+#msg_ios = '{"aps":{"alert":"iOS Message from Push","sound":"","badge":1},"key1":"value1","key2":"value2"}'
+opts = {'msg_type':1, 'expires':300} 
+
+c = Channel()
+
+try:
+    ret = c.pushMsgToSingleDevice(channel_id, msg, opts)
+    print('ret: ', end=' ')
+    print(ret)
+    print(c.getRequestId())
+except ChannelException as e:
+    print('[code]', end=' ')
+    print(e.getLastErrorCode())
+    print('[msg]', end=' ')
+    print(e.getLastErrorMsg())
+    print('[request id]', end=' ')
+    print(c.getRequestId())
+
+try:
+    ret = c.pushMsgToAll(msg, opts)
+    print('ret: ', end=' ')
+    print(ret)
+    print(c.getRequestId())
+except ChannelException as e:
+    print('[code]', end=' ')
+    print(e.getLastErrorCode())
+    print('[msg]', end=' ')
+    print(e.getLastErrorMsg())
+    print('[request id]', end=' ')
+    print(c.getRequestId())
+
+try:
+    ret = c.pushMsgToTag('test_tag', msg, 1, opts)
+    print('ret: ', end=' ')
+    print(ret)
+    print('[request id]', end=' ')
+    print(c.getRequestId())
+except ChannelException as e:
+    print('[code]', end=' ')
+    print(e.getLastErrorCode())
+    print('[msg]', end=' ')
+    print(e.getLastErrorMsg())
+    print('[request id]', end=' ')
+    print(c.getRequestId())
